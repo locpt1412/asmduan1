e2daea2fd5be8445bd425f6f5df47cb4f8e84a01
Pham Tan Loc <locptps24674@fpt.edu.vn>
2022-12-09 09:30:51 +0700

    load

--- view/footer.php
+++ view/footer.php
@@ -3,7 +3,7 @@
 <footer>
      <img src="./view/images/logo.png" width="100" height="13%">
      <div class="address">
-     <div class="thongtin">
+     <div class="thongtin" >
         Trung tâm DVKH <br>
          Vĩnh Khánh, xã An Thạnh, huyện Mỏ Cày Nam, tỉnh Bến Tre <br>
         Hotline: <a href="#">0372757845</a>
@@ -14,7 +14,7 @@
          <a href="#"><img src="./view/images/Twitter-logo.svg.png" width="50px" height="50">Twitter</a>
       </div>
       <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.867171118986!2d106.30569781445885!3d10.109957092775812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a00fa7c0fa5fdd%3A0x8c423d29406c8691!2zQ2jhu6MgVMOibiBYdcOibg!5e0!3m2!1sen!2s!4v1654771261951!5m2!1sen!2s" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3927.867171118986!2d106.30569781445885!3d10.109957092775812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a00fa7c0fa5fdd%3A0x8c423d29406c8691!2zQ2jhu6MgVMOibiBYdcOibg!5e0!3m2!1sen!2s!4v1654771261951!5m2!1sen!2s"  width="1100" height="200" style="border:0;"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     </div>
        </footer>
